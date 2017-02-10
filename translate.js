5d31e545f36e1fae0d2a47c75ce59425d2f0b735
Neson <neson@dex.tw>
2017-02-10 15:17:32 +0800

    update to use google-translate-api

---
+++ translate.js
@@ -0,0 +1,39 @@
+#!./node
+var translate = require('google-translate-api');
+
+var source = process.argv[2]
+var target = process.argv[3]
+var query = process.argv[4]
+var target_display_name = process.argv[5]
+
+translate(query, { from: source, to: target, raw: true }).then(res => {
+  var rawStr = res.raw.replace(/,,/g, ',null,').replace(/,,/g, ',null,').replace(/\[,/g, '[null,');
+  var raw = JSON.parse(rawStr);
+
+  output = { items: [] };
+
+  if (res.from.text.autoCorrected) {
+    var autoCorrected = res.from.text.value.replace(/\<[^\<\>]+\>/g, '');
+    autoCorrected = autoCorrected.replace(/^\[/, '').replace(/\]$/, '')
+    output.items.push({ title: autoCorrected, subtitle: 'Did you mean this?', autocomplete: autoCorrected });
+  }
+
+  if (res.text) {
+    output.items.push({ title: res.text, subtitle: target_display_name, arg: res.text });
+  }
+
+  if (raw && raw[1]) {
+    raw[1].forEach((group) => {
+      var type = group[0];
+
+      group[2].forEach((wordData) => {
+        var word = wordData[0];
+        var translations = wordData[1];
+        var frequency = wordData[3];
+        output.items.push({ title: word, subtitle: `(${type}) ${translations.join(', ')}`, arg: word });
+      });
+    });
+  }
+
+  console.log(JSON.stringify(output));
+});
